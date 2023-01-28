a292682853d22c0f5382bc311138a5a4f35ac362
whatsappbot-dt <116092838+whatsappbot-dt@users.noreply.github.com>
2023-01-28 13:59:55 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -32,20 +32,20 @@ global.APIKeys = {
 }
 
 //other
-global.owner = ['94715166712'] // Owner number , (අයිතිකරුගේ නම්බර් එක )
+global.owner = ['94761544734'] // Owner number , (අයිතිකරුගේ නම්බර් එක )
 global.pemilik = ['94715166712'] //Change  it , ඔබට කැමති නම් වෙනස් කරන්න
 global.premium = ['94715166712'] //Change it ඔබට කැමතිනම් වෙනස් කරන්න 
-global.pengguna = 'Dark Maker' // Your name ඔබේ නම 
-global.botnma = '💃 𝐐𝐮𝐞𝐞𝐧 𝐄𝐥𝐢𝐬𝐚 𝐁𝐨𝐭 💃' // Your bot name , ඔබගෙ බොට්ගේ නම 
-global.ownernma = '𝓓𝓪𝓻𝓴 𝓜𝓪𝓴𝓮𝓻' //ownernama,ownername
-global.packname = 'Queen Elisa Sticker' // Sticker package name 
+global.pengguna = 'AMESH DENUWAN' // Your name ඔබේ නම 
+global.botnma = '💃 DENUWAN BOT 💃' // Your bot name , ඔබගෙ බොට්ගේ නම 
+global.ownernma = 'Amesh Denuwan' //ownernama,ownername
+global.packname = 'DENUWAN BOT STICKERS' // Sticker package name 
 global.author = 'Dark Maker' // Sticker Autor name 
 global.sessionName = 'session'
 global.prefa = ['#','!','/',''] 
 global.sp = '🔵'
 // Alive massage , ඔබේ බොගේ alive massage  එක මෙහි ඇති ` ` මෙම ලකුනු ඇතුලේ ඔබට අවශ්‍ය ඔනිම දෙයක් Type කරන්න.💃
 global.alivelogo = `https://telegra.ph/file/1a2dee71baa975091620d.jpg`
-global.alive =`Hello i am alive now
+global.alive =`Hello your denuwan bot alive now😍
 
 Download song type .yt 
 Thanks fro using...`
